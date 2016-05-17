991083c33a0d5a7f07911c8ed89d3fbfc5ff5d75
skyclan9 <skyclan9@aol.com>
2016-05-17 13:57:58 -0400

    made game_board class and added pics for tiles

--- src/game_board.py
+++ src/game_board.py
@@ -1,1 +1,28 @@
 import pygame
+
+class GameBoard:
+    """
+    Game Board class for PyChess
+    """
+    def __init__(self):
+        self.width = 8
+        self.height = 8
+        self.tileArray = [[0 for x in range(self.width)] for y in range(self.height)]
+        self.rectArray = [[0 for x in range(self.width)] for y in range(self.height)]
+        
+    def fill_tile_array(self):
+        blackTile = Tile("black", "../resources/black.jpg")
+        whiteTile = Tile("white", "../resources/white.jpg")
+        for i in range(8):
+            for k in range(8):
+               if i%2==0: 
+                    if k%2==0:
+                        tile[i,k] = whiteTile
+                    else:
+                        tile[i,k] = blackTile
+                else:
+                    if k%2==0:
+                        tile[i,k] = blackTile
+                    else:
+                        tile[i,k] = whiteTile
+        
