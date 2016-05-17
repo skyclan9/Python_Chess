ed1f04d851bda78b40619b056e1362aeb64529d8
skyclan9 <skyclan9@aol.com>
2016-05-17 12:55:40 -0400

    added classes

--- src/test.py
+++ src/test.py
@@ -1,23 +1,13 @@
-import sys, pygame
+import sys
+import pygame
 pygame.init()
 
-size = width, height = 320, 240
-speed = [2,2]
-black = 0,0,0
-
-screen = pygame.display.set_mode(size)
-
-ball = pygame.image.load("ball.png") #not gonna work cuz that pic is gone
-ballrect = ball.get_rect()
-
+size = width, height = 722, 722
+surface = pygame.display.set_mode(size)
+background = pygame.image.load("../resources/chessboard_722.png").convert()
+backgroundrect = background.get_rect()
+surface.blit(background, backgroundrect)
+pygame.display.update(backgroundrect)
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:   sys.exit()
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
