b16c5168b6da165febc1a807226bc6663b747a7b
skyclan9 <skyclan9@aol.com>
2016-05-17 13:17:12 -0400

    made game piece and tile class

--- src/game_piece.py
+++ src/game_piece.py
@@ -1,1 +1,20 @@
 import pygame
+
+class GamePiece:
+    "Game Piece Class"
+    def __init__(self, name, color, image):
+        self.name = name
+        self.color = color
+        self.image = image
+        
+    def get_name(self):
+        return self.name
+        
+    def get_color(self):
+        return self.color
+           
+    def get_image(self):
+        return self.image
+        
+    
+        
--- src/tile.py
+++ src/tile.py
@@ -1,5 +1,15 @@
 import pygame
 
-class tile:
+class Tile:
+    'Tile Class'
+    def __init__(self, color, image):
+        self.color = color
+        self.image = image
+        
+    def get_color(self):
+        return self.color
+        
+    def get_image(self):
+        return self.image
+        
     
-    def __init__(self, i)
